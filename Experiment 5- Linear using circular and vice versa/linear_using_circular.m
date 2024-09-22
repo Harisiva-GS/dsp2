fc83eec492ec7ffd3f9466988ce4f16e853df5d3
Harisiva <220081@tkmce.ac.in>
2024-09-22 03:39:53 +0000

    Performed Linear Convolution using Circular Convolution.

---
+++ Experiment 5- Linear using circular and vice versa/linear_using_circular.m
@@ -0,0 +1,11 @@
+%Linear convolution using circular convolution
+x = [1, 2, 3];
+h = [4, 5, 6];
+len_x = length(x);
+len_h = length(h);
+N = len_x + len_h - 1;
+x_padded = [x, zeros(1, N - len_x)];
+h_padded = [h, zeros(1, N - len_h)];
+y_circular = cconv(x_padded, h_padded, N);
+disp('The result of linear convolution using circular convolution:');
+disp(y_circular);
