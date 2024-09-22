6359c8e99345cc9bb071d99e6939e205704f6cd3
itshabeeb <habeebmniyas04@gmail.com>
2024-09-22 23:47:20 +0530

    Output made more reliable

--- Experiment 3- Linear Convolution/linear_no_builtin.m
+++ Experiment 3- Linear Convolution/linear_no_builtin.m
@@ -1,9 +1,36 @@
-%Q1
-% Linear Convolution of x(n)=[1 2 1 2]  , h(n)=[3 2 1 2]
-
 clc;
 clear;
-close;
+close all;
+
+%Q1
+
+%linear convolution of x(n)=[1 2 1 1]  , h(n)=[1 1 1 1]
+
+x = [1, 2, 1, 1];
+h = [1, 1, 1, 1];
+len_x = length(x);
+len_h = length(h);
+len_y = len_x + len_h - 1;
+y = zeros(1, len_y);
+for i = 1:len_x
+    for j = 1:len_h
+        y(i+j-1) = y(i+j-1) + x(i) * h(j);
+    end
+end
+disp('Linear Convolution of x(n)=[1 2 1 1]  , h(n)=[1 1 1 1]');
+disp(y);
+n = 0:len_y-1; 
+subplot(1,2,1)
+stem(n, y, 'filled'); 
+title('Linear Convolution x(n)=[1 2 1 1]  , h(n)=[1 1 1 1]');
+xlabel('n');
+ylabel('y(n)');
+grid on;
+
+%Q2
+
+% Linear Convolution of x(n)=[1 2 1 2]  , h(n)=[3 2 1 2]
+
 xn1 = [1 2 1 2];
 hn1 = [3 2 1 2];
 l1 = length(xn1);
@@ -20,34 +47,12 @@ for i = 1:p
     y(i) = sum;
 end
 p_range = 0:p-1;
+disp('Linear Convolution of x(n)=[1 2 1 2]  , h(n)=[3 2 1 2]')
 disp(y)
+subplot(1,2,2)
 stem(p_range,y);
 axis([-2 2 0 12]);
 xlabel('n');
 ylabel('Amplitude');
-title('Linear Convolution');
-
-%Q2
-%linear convolution of x(n)=[1 2 1 1]  , h(n)=[1 1 1 1]
-
-x = [1, 2, 1, 1];
-h = [1, 1, 1, 1];
-len_x = length(x);
-len_h = length(h);
-len_y = len_x + len_h - 1;
-y = zeros(1, len_y);
-for i = 1:len_x
-    for j = 1:len_h
-        y(i+j-1) = y(i+j-1) + x(i) * h(j);
-    end
-end
-disp('Linear Convolution Result:');
-disp(y);
-n = 0:len_y-1; 
-figure;
-stem(n, y, 'filled'); 
-title('Linear Convolution of x(n) and h(n)');
-xlabel('n');
-ylabel('y(n)');
-title("Linear Convolution")
+title('Linear Convolution of x(n)=[1 2 1 2]  , h(n)=[3 2 1 2]');
 grid on;
