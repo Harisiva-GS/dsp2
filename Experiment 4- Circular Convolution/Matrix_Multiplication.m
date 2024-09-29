684e058396b3b8d02165821bcc08090fb0960115
itshabeeb <habeebmniyas04@gmail.com>
2024-09-29 23:02:56 +0530

    Included the visual output to the matrix method of circular cinv

--- Experiment 4- Circular Convolution/Matrix_Multiplication.m
+++ Experiment 4- Circular Convolution/Matrix_Multiplication.m
@@ -1,15 +1,36 @@
-%Circular convolution using matrix multiplication
 clc;
+clear;
 close all;
-clear all;
-xn=[2 1 2 1];
-hn=[1 2 3 4];
-h=[];
-hn=hn(:,end:-1:1);
-for i=1:length(hn)
- hn=[hn(end) hn(1:end-1)];
- h=[h;hn];
+x=input('enter the elements in x[n]:');
+x_ind=input('enter the index of x[n]:');
+h=input('enter the elements in h[n]:');
+h_ind=input('enter the index of h[n]:');
+hr=[];
+h1=h;
+h=h(:,end:-1:1);
+for i=1:length(h)
+h=[h(end) h(1:end-1)];
+hr=[hr;h];
 end
-y=h*xn';
-disp("Convolution product y:")
+y=hr*x';
 disp(y);
+subplot(3,1,1);
+stem(x_ind,x);
+title('x[n]');
+xlabel('time');
+ylabel('amplitude');
+grid;
+subplot(3,1,2);
+stem(h_ind,h1);
+title('h[n]');
+xlabel('time');
+ylabel('amplitude');
+grid;
+subplot(3,1,3);
+Ny=0:3;
+stem(Ny,y);
+title('Circular convolution output y[n]');
+xlabel('time');
+ylabel('amplitude');
+grid;
+
