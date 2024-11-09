b9c185a2e40df576ae951cf7b8520f72e953a91c
itshabeeb <habeebmniyas04@gmail.com>
2024-11-09 23:36:25 +0530

    Exceuted Overlap Add and Save Method

--- Experiment 8- Overlap Add and Save Method/overlapadd_.m
+++ Experiment 8- Overlap Add and Save Method/overlapadd_.m
@@ -1,32 +1,51 @@
 clc;
 clear;
 close all;
-x=input("enter the elements of x: ");
-h= input("enter the elements of h: ");
-N=5;
+% Step 1: Define the Input Signal and Filter
+Xn = input("enter the elements of x: ");  
+Hn = input ("enter the elements of h: ");                         
+L = 5;                                  
 
-% Code to perform Convolution using Overlap Save Method
-lx=length(x);
-lh=length(h);
-m=lh-1;
-x=[zeros(1,m) x zeros(1,lx+lh-1)];
-disp(x);
-h=[h zeros(1,N-lh)];
-L=N-lh+1;
-k=5;
-for i=0:k-1
-    y=x(1,i*L+1:i*L+N);
-    disp(y);
-    q=mycirconv1(y,h);  %Call the mycirconv1 function.
-    p(i+1,:)=q;
+NXn = length(Xn);                     
+M = length(Hn);                        
+M1 = M - 1;                           
+R = rem(NXn, L);                      
+N = L + M1;                           
+
+% zero padding
+Xn = [Xn, zeros(1, L - R)];          
+Hn = [Hn, zeros(1, N - M)];          
+
+ %overlap add method
+K = floor(length(Xn) / L);           
+y = zeros(1, length(Xn) + M - 1);     
+z = zeros(1, M1);                     
+
+for k = 0:K-1
+    startIndex = L * k + 1;
+    endIndex = min(startIndex + L - 1, length(Xn));
+    Xnp = Xn(startIndex:endIndex);
+
+    Xnk = [Xnp, z]; 
+    convSegment = mycirconv(Xnk, Hn);
+
+    % Add the current segment to the output
+    outputStart = startIndex;
+    outputEnd = outputStart + N - 1;
+    y(outputStart:outputEnd) = y(outputStart:outputEnd) + convSegment(1:N);
 end
-p1=p(:,lh:N)';
-p=p1(:)';
+
+
+disp('Input Signal:');
+disp(Xn);
+disp('Filter:');
+disp(Hn);
+disp('Output Signal (Convolved using Overlap-Add Method):');
+disp(y);
 
 % Function for Circular Convolution
-function y = mycirconv1(x, h)
+function y = mycirconv(x, h)
     % Compute the circular convolution using FFT
-    N = length(x);
-    y = ifft(fft(x) .* fft(h, N)); % FFT-based circular convolution
+    N = max(length(x), length(h));
+    y = ifft(fft(x, N) .* fft(h, N)); % FFT-based circular convolution
 end
-disp(p);
